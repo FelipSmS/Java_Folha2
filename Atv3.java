6fbddbf27da50f05b586adb4643c6f37a8c07cd6
FelipeSmS <felpz.gg@gmail.com>
2024-03-22 19:38:56 -0300

    Lista 2 - Atv3

---
+++ Atv3.java
@@ -0,0 +1,31 @@
+//3. Faça um programa que leia em um vetor uma sequência finita de números digitadospelo usuário.
+//Crie um segundo vetor que armazene o dobro de cada número doprimeiro vetor e depois apresente os valores deste vetor.
+
+import java.util.Scanner;
+
+public class Atv3 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Digite o tamanho do vetor: ");
+        int tamanho = ler.nextInt();
+
+        int[] vetor1 = new int[tamanho];
+
+        System.out.println("Digite os números para o primeiro vetor:");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print("Posição " + i + ": ");
+            vetor1[i] = ler.nextInt();
+        }
+
+        int[] vetor2 = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            vetor2[i] = vetor1[i] * 2;
+        }
+
+        System.out.println("Valores do segundo vetor (dobro dos valores do primeiro vetor):");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.println("Posição " + i + ": " + vetor2[i]);
+        }
+    }
+}
